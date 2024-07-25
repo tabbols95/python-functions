8bc66b59e28c1e8d4f414eca9fd83c909693b689
Alexander Shilov <tabbols95@gmail.com>
2024-07-26 02:54:57 +0300

    Create convert.py
    create convert.py and add function to_decimal

---
+++ convert.py
@@ -0,0 +1,12 @@
+from decimal import *
+from typing import Optional
+from checks import _check_is_numeric
+
+def to_decimal(number, prec: int=2) -> Optional[Decimal]:
+    getcontext().prec = prec
+
+    flag, number = _check_is_numeric(number)
+    if flag:
+        return Decimal(number)
+    else:
+        return
